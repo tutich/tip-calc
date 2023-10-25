31c0f19922f1618ec878dc2bdd028d172d8dfc2d
tutich <pkoinange02@gmail.com>
2023-10-25 12:18:40 +0300

    body css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './components/images/logo.svg';
 import Form from './components/Form';
 import Display from './components/Display';
 
@@ -8,6 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <div className="App">
+      <img src={logo} alt='splitter'/>
       <div class="wrapper">
         <div className='container'>
           <div className='form'>
