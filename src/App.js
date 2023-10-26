56c38f7dd96fffeb37853ac4c16914d11e0c2e2f
tutich <pkoinange02@gmail.com>
2023-10-26 18:01:13 +0300

    js usestate

--- src/App.js
+++ src/App.js
@@ -5,16 +5,46 @@ import Form from './components/Form';
 import Display from './components/Display';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState("");
+  const [people, setPeople] = useState("");
+  const [calculatedTip, setCalculatedTip] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if(bill > 0 && people > 0 && tip > 0){
+      setCalculatedTip(bill * (tip / 100));
+      setTotal(calculatedTip + bill)
+    }
+  }, [bill, people, tip, calculatedTip])
+
+  const handleResetBtn = (e) => {
+    setBill("");
+    setTip("");
+    setPeople("");
+    setCalculatedTip(0);
+    setTotal(0);
+  }
   return (
     <div className="App">
-      <img src={logo} alt='splitter'/>
+      <img src={logo} alt='splitter' className='log'/>
       <div class="wrapper">
         <div className='container'>
-          <div className='form'>
-          </div>
-          <Form />
-          <Display />
+    
+          <Form 
+          bill={bill}
+          setBill={setBill}
+          setTip={setTip}
+          people={people}
+          setPeople={setPeople}
+          />
+          <Display 
+          total={total}
+          people={people}
+          calculatedTip={calculatedTip}
+          handleResetBtn={handleResetBtn}
+          />
         </div>
       </div>
       
